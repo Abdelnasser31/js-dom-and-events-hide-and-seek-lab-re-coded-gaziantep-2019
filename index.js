5c22829bf47dc5261a80f3be3ba598fb8112aa8e
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-19 08:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function increaseRankBy(n) {
   const secondRankedLIst = rankedLists[1];
   let children = firstRankedList.children;
   
-  for( let i = 0 ; i< children.length; i++ ) {
-    children[i] = parseInt(children[i].innerHTML) + n; 
+  for( let i = 0 ; i < children.length; i++ ) {
+    children[i].innerHTML = parseInt(children[i].innerHTML) + n; 
   }
   
 }
